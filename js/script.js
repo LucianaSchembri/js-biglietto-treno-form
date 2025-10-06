32648abed621d52f0d572d0fbf6c40d1ff9114a5
LucianaSchembri <lucy.sche@gmail.com>
2025-10-06 21:46:46 +0200

    feat: impostati elementi DOM

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 
+formElem.addEventListener ("submit", function(event) { 
+    event.preventDefault();
+})
+
+const formElem = document.querySelector ("form")
+const numeroKm = document.getElementById ("kmUtente")
+const utenteEta = document.getElementById ("etaUtente")
+
